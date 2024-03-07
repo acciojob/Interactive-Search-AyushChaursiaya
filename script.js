ae6c08917c712e5000713d3f40e0da610f65b0f4
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-03-07 23:31:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+  var searchButton = document.getElementById('searchButton');
+  var searchContainer = document.getElementById('searchContainer');
+  var searchInput = document.getElementById('searchInput');
+  
+  searchButton.addEventListener('click', function() {
+    searchContainer.classList.add('active');
+    searchInput.focus();
+  });
+});
